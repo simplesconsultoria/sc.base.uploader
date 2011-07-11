4b532839375f70bb399ba548fc6f53d1d53388db
Erico Andrei <erico@simplesconsultoria.com.br>
2011-07-11 17:11:52 -0300

    Invoke patch

--- sc/base/uploader/__init__.py
+++ sc/base/uploader/__init__.py
@@ -3,5 +3,9 @@
 from zope.i18nmessageid import MessageFactory as BaseMessageFactory
 MessageFactory = BaseMessageFactory('sc.base.uploader')
 
+from sc.base.uploader import patch
+
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
+patch.run()
