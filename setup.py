1fbecd7a1a6e60bca8bfb25b5bb7c703d0d89ae7
Erico Andrei <erico@simplesconsultoria.com.br>
2012-03-05 20:50:18 -0300

    PEP8 and pyflakes

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@
 from setuptools import setup, find_packages
 import os
 
-version = open(os.path.join("sc", "base", "uploader", "version.txt")).read().strip()
+version = open(os.path.join("sc", "base", "uploader",
+                            "version.txt")).read().strip()
 
 setup(name='sc.base.uploader',
       version=version,
       description="A Plone package providing multiple-files upload",
-      long_description=open(os.path.join("sc", "base", "uploader", "README.txt")).read() + "\n" +
+      long_description=open(os.path.join("sc", "base", "uploader",
+                                         "README.txt")).read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Framework :: Zope2",
@@ -20,7 +23,7 @@ setup(name='sc.base.uploader',
       keywords='upload massuploader zip plone',
       author='Simples Consultoria',
       author_email='products@simplesconsultoria.com.br',
-      url='http://svn.plone.org/svn/collective/',
+      url='https://bitbucket.org/simplesconsultoria/sc.base.uploader',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['sc', 'sc.base'],
@@ -39,4 +42,3 @@ setup(name='sc.base.uploader',
       target = plone
       """,
       )
-
