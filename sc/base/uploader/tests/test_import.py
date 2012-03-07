8a5d48b1289ad89aa8601e3473333c8c5646e837
Erico Andrei <erico@simplesconsultoria.com.br>
2012-03-06 19:56:38 -0800

    Add import tests

---
+++ sc/base/uploader/tests/test_import.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import os
+import unittest2 as unittest
+
+from zope.component import getUtility
+
+from plone.app.testing import TEST_USER_ID
+from plone.app.testing import setRoles
+
+from collective.zipfiletransport.utilities.interfaces import (
+                                                IZipFileTransportUtility)
+
+from sc.base.uploader.testing import INTEGRATION_TESTING
+
+
+class FilenamesExportTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.pp = self.portal['portal_properties']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        props = self.pp.sc_base_uploader
+        props.manage_changeProperties(enable_export=True)
+        self.portal.invokeFactory('Folder',
+                                  'f1',
+                                  title=u"Folder 1")
+        self.folder = self.portal['f1']
+
+    def test_import_zip(self):
+        zft_util = getUtility(IZipFileTransportUtility,
+                              name="sc_zipfiletransport")
+        file_path = os.path.join(os.path.dirname(__file__), 'foobar.zip')
+        zft_util.importContent(
+                                file=file_path,
+                                context=self.folder,
+                                description='test_folder zip file description',
+                                contributors='test_user',
+                                overwrite=False,
+                                categories=[],
+                                excludefromnav=False,
+                                )
+        folder = self.folder
+        self.assertEqual(folder.objectIds(), ['foobar'])
+        files = folder['foobar'].objectIds()
+        self.assertTrue('test_image.png' in files)
+        self.assertTrue('test_image.jpg' in files)
+
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
