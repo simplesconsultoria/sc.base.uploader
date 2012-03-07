c5aa181429078246103ef99cd5461fb55a369db8
Erico Andrei <erico@simplesconsultoria.com.br>
2012-03-06 19:56:31 -0800

    Improve export tests

--- sc/base/uploader/tests/test_export.py
+++ sc/base/uploader/tests/test_export.py
@@ -2,17 +2,22 @@
 import os
 import unittest2 as unittest
 
-from zope.app.component.hooks import getSite
+from zope.component import getUtility
 
 from plone.testing.z2 import Browser
 
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_NAME, TEST_USER_PASSWORD
+
+from collective.zipfiletransport.utilities.interfaces import (
+                                                IZipFileTransportUtility)
+
+from sc.base.uploader.testing import INTEGRATION_TESTING
 from sc.base.uploader.testing import FUNCTIONAL_TESTING
 
 
-class ExportTest(unittest.TestCase):
+class BaseExportTest(unittest.TestCase):
 
     layer = FUNCTIONAL_TESTING
 
@@ -31,7 +36,7 @@ class ExportTest(unittest.TestCase):
             image.reindexObject()
 
     def setUp(self):
-        self.app = self.layer['app']        
+        self.app = self.layer['app']
         self.portal = self.layer['portal']
         self.pp = self.portal['portal_properties']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
@@ -85,6 +90,73 @@ class ExportTest(unittest.TestCase):
                          'zip-exporsct-42')
         self.assertEqual(browser.headers['Cache-Control'],
                          'max-age=86400, proxy-revalidate, public')
+
+
+class FilenamesExportTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUpContent(self):
+        self.portal.invokeFactory('Folder',
+                                  'f1',
+                                  title=u"Folder 1")
+        self.folder = self.portal['f1']
+        files = ['test_image.png', 'test_image.jpg', ]
+        for filename in files:
+            image_file = open(os.path.join(os.path.dirname(__file__),
+                              filename))
+            self.folder.invokeFactory('Image', filename)
+            image = self.folder[filename]
+            image.setImage(image_file.read())
+            image.reindexObject()
+
+    def setUpImagesWithNoExtension(self):
+        # Add a file with no extension
+        filename = 'test_image.png'
+        image_file = open(os.path.join(os.path.dirname(__file__),
+                              filename))
+        self.folder.invokeFactory('Image', 'image_without_extension')
+        image = self.folder[filename]
+        image.setImage(image_file.read())
+        image.reindexObject()
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.pp = self.portal['portal_properties']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        props = self.pp.sc_base_uploader
+        props.manage_changeProperties(enable_export=True)
+        self.setUpContent()
+
+    def test_export_files(self):
+        zft_util = getUtility(IZipFileTransportUtility,
+                              name="sc_zipfiletransport")
+        obj_paths = ['/'.join(self.folder.getPhysicalPath())]
+        zip_path = zft_util.exportContent(self.folder, obj_paths)
+        fp = open(zip_path, 'rb')
+        filenames = zft_util.getZipFilenames(fp)
+        self.assertEqual(len(filenames), 2)
+
+    def test_export_filenames(self):
+        zft_util = getUtility(IZipFileTransportUtility,
+                              name="sc_zipfiletransport")
+        obj_paths = ['/'.join(self.folder.getPhysicalPath())]
+        zip_path = zft_util.exportContent(self.folder, obj_paths)
+        fp = open(zip_path, 'rb')
+        filenames = zft_util.getZipFilenames(fp)
+        self.assertTrue('test_image.png' in filenames)
+        self.assertTrue('test_image.jpg' in filenames)
+
+    def test_export_filenames_with_no_extension(self):
+        self.setUpImagesWithNoExtension()
+        zft_util = getUtility(IZipFileTransportUtility,
+                              name="sc_zipfiletransport")
+        obj_paths = ['/'.join(self.folder.getPhysicalPath())]
+        zip_path = zft_util.exportContent(self.folder, obj_paths)
+        fp = open(zip_path, 'rb')
+        filenames = zft_util.getZipFilenames(fp)
+        self.assertEqual(len(filenames), 3)
+        self.assertTrue('image_without_extension.png' in filenames)
 
 
 def test_suite():
