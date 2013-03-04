f475283b3a70b503905f88cec875d4860840c487
Érico Andrei <erico@simplesconsultoria.com.br>
2013-03-04 14:18:25 -0300

    Fix package name

--- pre-commit-check.sh
+++ pre-commit-check.sh
@@ -15,7 +15,7 @@ echo '====== Running PyFlakes ======'
 bin/zopepy setup.py flakes; handle_exit
 
 echo '====== Running pep8 =========='
-bin/pep8 --ignore=E501 src/sc/base/upload; handle_exit
+bin/pep8 --ignore=E501 src/sc/base/uploader; handle_exit
 bin/pep8 --ignore=E501 setup.py; handle_exit
 
 if [ $EXITCODE -ne 0 ]; then
