1343491944408886cd5d7f5beec29757252f345a
Érico Andrei <erico@simplesconsultoria.com.br>
2013-03-05 15:51:27 -0300

    Remove hard linked dependencies

--- src/sc/base/uploader/config.py
+++ src/sc/base/uploader/config.py
@@ -6,8 +6,3 @@ __author__ = """Simples Consultoria <products@simplesconsultoria.com.br>"""
 __docformat__ = 'plaintext'
 
 PROJECTNAME = 'sc.base.uploader'
-
-# (name,locked,hidden,install,profile,runProfile)
-
-PRODUCTS = [('collective.quickupload', 0, 1, 1, 'collective.quickupload:default', 0),
-            ('collective.zipfiletransport', 0, 1, 1, 'collective.zipfiletransport:default', 1)]
