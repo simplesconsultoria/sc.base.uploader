b017304d9c17812cb268b73ee958a5c120ab4229
Érico Andrei <erico@simplesconsultoria.com.br>
2013-03-05 16:26:58 -0300

    Back to development: 1.0b3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0b2'
+version = '1.0b3.dev0'
 
 long_description = (open("README.txt").read() + "\n" +
                     open(os.path.join("docs", "INSTALL.txt")).read() + "\n" +
